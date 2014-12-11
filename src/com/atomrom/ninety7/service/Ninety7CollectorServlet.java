9dbf6661e82c1fd7e3bbb8cd3ce8ceff84b9230c
Mormota <atomrom@atomrom.com>
2014-12-11 16:43:00 +0100

    simple collector service & persistence
    saves the basic browsing data:
    - url
    - timestamp
    - duration
    - title
    - content

---
+++ src/com/atomrom/ninety7/service/Ninety7CollectorServlet.java
@@ -0,0 +1,38 @@
+package com.atomrom.ninety7.service;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Text;
+
+@SuppressWarnings("serial")
+public class Ninety7CollectorServlet extends HttpServlet {
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		// UserService userService = UserServiceFactory.getUserService();
+		// User user = userService.getCurrentUser();
+
+		String urlVisited = req.getParameter("urlVisited");
+		String visitDuration = req.getParameter("visitDuration");
+		String title = req.getParameter("title");
+		String content = req.getParameter("content");
+				
+		Entity visit = new Entity("Visit");
+		visit.setProperty("timestamp",System.currentTimeMillis());
+		visit.setProperty("urlVisited", urlVisited);
+		visit.setProperty("visitDuration", visitDuration);
+		visit.setProperty("title", new Text(title));
+		visit.setProperty("content", new Text(content));
+		
+		DatastoreService datastore = DatastoreServiceFactory
+				.getDatastoreService();
+		datastore.put(visit);
+	}
+}
